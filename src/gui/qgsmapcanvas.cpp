2220cc45b9c695333f2867651ec718a1bb9f5842
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-09-21 07:46:51 +0200

    optimize scale for point layers in QgsMapCanvas::zoomToFeatureIds
    similarly to what is done in zoomToSelected

--- src/gui/qgsmapcanvas.cpp
+++ src/gui/qgsmapcanvas.cpp
@@ -1340,6 +1340,10 @@ void QgsMapCanvas::zoomToFeatureIds( QgsVectorLayer *layer, const QgsFeatureIds 
   QString errorMsg;
   if ( boundingBoxOfFeatureIds( ids, layer, bbox, errorMsg ) )
   {
+    if ( bbox.isEmpty() )
+    {
+      bbox = optimalExtentForPointLayer( layer, bbox.center() );
+    }
     zoomToFeatureExtent( bbox );
   }
   else
